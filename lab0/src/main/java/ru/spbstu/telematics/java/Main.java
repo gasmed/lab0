a4780849f23949d1173cf87b445dbd1585358aba
gasmed <47139104+gasmed@users.noreply.github.com>
2024-02-09 00:44:25 +0300

    Update and rename App.java to Main.java
    Updated lab0

--- lab0/src/main/java/ru/spbstu/telematics/java/Main.java
+++ lab0/src/main/java/ru/spbstu/telematics/java/Main.java
@@ -1,25 +1,24 @@
 package ru.spbstu.telematics.java;
 
-public class App {
+public class Main {
     public static void main(String[] args) {
         // Задаем параметры нормального распределения
         double mean = 10; // Среднее значение
         double standardDeviation = 1.0; // Стандартное отклонение
         // Генерируем случайное число с нормальным распределением
-	double randomValue = generateRandomNormal(mean, standardDeviation);
+        RandomNumberGenerator generator = new RandomNumberGenerator();
+        double randomValue = generator.generateRandomNormal(mean, standardDeviation);
 
         System.out.println("\nСлучайное число с нормальным распределением: " + randomValue);
     }
-    // Метод для генерации случайной величины с нормальным распределением
-    public static double generateRandomNormal(double mean, double standardDeviation) {
-        // Генерируем пару независимых случайных чисел с равномерным распределением
+}
+class RandomNumberGenerator {
+    public double generateRandomNormal(double mean, double standardDeviation) {
         double x1 = Math.random();
         double x2 = Math.random();
 
-        // Используем метод Бокса-Мюллера для получения пары независимых случайных чисел
         double z0 = Math.sqrt(-2.0 * Math.log(x1)) * Math.cos(2.0 * Math.PI * x2);
 
-        // Масштабируем и сдвигаем полученные числа
         return mean + standardDeviation * z0;
     }
 }
